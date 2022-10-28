92cb7030f6db7c0c3490016032cd70984af82a1e
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-28 19:59:08 +0200

    wip without styles

--- JS/script.js
+++ JS/script.js
@@ -3,27 +3,60 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return {
+            newTask: {
+                text:'',
+                done: false,
+            },
+            hasError: false,
             tasks:[
                 {
                     text: 'to do 1',
-                    done: false
+                    done: false,
                 },
                 {
                     text: 'to do 2',
-                    done: true
+                    done: true,
                 },
                 {
                     text: 'to do 3',
-                    done: false
+                    done: false,
                 }
-            ]
+            ],
+            computed: {
+                incomplete(){
+                    return this.tasks.filter(this.inProgress).length;
+                }
+            }
         }
     },
     methods:{
+        addTask(){
+            if(this.newTask.text.length >= 3){
 
-    },
-    computed:{
+                this.tasks.push(this.newTask);
+                this.hasError = false;
 
+            }else{
+
+                this.hasError = true; 
+            }
+            // this.newTask = '';
+        },
+        removeTask(i){
+            this.tasks.splice(i,1);
+        },
+        inProgress(){
+            return !this.isCompleted(task);
+        },
+        isCompleted(task){
+            return task.done;
+        },
+        clearCompleted(){
+            this.tasks = this.tasks.filter(this.inProgress);
+        },
+        clearAll(){
+            this.tasks = [];
+        }
     },
     mounted(){
         console.log('in mounted');
