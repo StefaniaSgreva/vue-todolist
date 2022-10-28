69123ac187b6c5f690f29a98bada5bc3e7e82338
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-28 16:09:19 +0200

    added v-for (without styles)

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,20 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return {
-
+            tasks:[
+                {
+                    text: 'to do 1',
+                    done: false
+                },
+                {
+                    text: 'to do 2',
+                    done: true
+                },
+                {
+                    text: 'to do 3',
+                    done: false
+                }
+            ]
         }
     },
     methods:{
